eeeb0e0387588493b90fc45ef9f946640d95a9f9
silencema105 <js.mjj@aisino.sh.cn>
2018-08-18 22:49:07 +0800

    去掉注解

--- src/com/cn/majj/Login.java
+++ src/com/cn/majj/Login.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
-@WebServlet("/Login")
+//@WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
