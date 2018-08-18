0d4667330a36e27c331047c60cf1aecca3695f3f
silencema105 <js.mjj@aisino.sh.cn>
2018-08-18 22:42:29 +0800

    init

---
+++ src/com/cn/majj/Login.java
@@ -0,0 +1,56 @@
+package com.cn.majj;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8"); 
+		  response.setContentType("text/html;charset=utf-8"); 
+		String name = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (name.equals("mjj")  && password.equals("123")) {
+			response.getWriter().write("µÇÂ¼³É¹¦");
+			
+		} else {
+			response.getWriter().write("µÇÂ¼Ê§°Ü");
+			
+		}
+
+	}
+
+}
