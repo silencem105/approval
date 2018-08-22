c9e8158b2ca308e2070d8420d785477f0dae3672
silencema105 <js.mjj@aisino.sh.cn>
2018-08-22 23:08:25 +0800

    修改查询sql语句

---
+++ src/com/cn/majj/DbUtil.java
@@ -0,0 +1,45 @@
+package com.cn.majj;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+
+public class DbUtil {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		DbUtil.getUserByName("mjj", "123");
+	}
+
+	public static boolean getUserByName(String userName, String password) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		User user = new User();
+		String URL = "jdbc:mysql://127.0.0.1:3306/approval";
+		String jdbc_user = "root";
+		String jdbc_password = "111111";
+		
+		// 加载驱动程序
+		Class.forName("com.mysql.jdbc.Driver");
+		// 获取数据库连接
+		Connection con = DriverManager.getConnection(URL, jdbc_user, jdbc_password);
+		// 创建Statament对象
+		Statement st = con.createStatement();
+		String s = "select user_name,password from tbl_user where user_name=? and password=?";
+		PreparedStatement pst = con.prepareStatement(s);
+		pst.setString(1, userName);
+		pst.setString(2, password);
+		ResultSet rst = pst.executeQuery();
+		// 处理数据库的返回结果(使用ResultSet类)
+		while (rst.next()) {
+			System.out.println(rst.getString("user_name") + " " + rst.getString("password"));
+			return true;
+		}
+		rst.close();
+		pst.close();
+		con.close();
+		return false;
+	}
+
+}
---
+++ src/com/cn/majj/User.java
@@ -0,0 +1,20 @@
+package com.cn.majj;
+
+public class User {
+private int user_id;
+private String user_name;
+private String password;
+public String getName(){
+	return user_name;
+}
+public void setName(){
+	this.user_name=user_name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
