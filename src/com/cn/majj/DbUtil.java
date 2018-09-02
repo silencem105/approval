582850682fb8ae31ff2519884c02287c16f058b6
silencema105 <js.mjj@aisino.sh.cn>
2018-09-02 22:40:38 +0800

    更新单个用户

--- src/com/cn/majj/DbUtil.java
+++ src/com/cn/majj/DbUtil.java
@@ -11,29 +11,41 @@ import java.sql.Connection;
 public class DbUtil {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		
-		//DbUtil.getUserByName("mjj", "123");
-		//User user=new User();
-		//user.setUserId(UUID.randomUUID().toString());
-		//user.setUserName("zkn");
-		//user.setPassword("111");
-		User u=DbUtil.queryUsers("2009620d-32ae-472f-becc");
-		System.out.println(u.getUserId());
-		System.out.println("姓名:"+u.getUserName());
-		System.out.println("密码:"+u.getPassword());
+		// 根据用户名称查询用户
+		/* DbUtil.getUserByName("mjj", "123"); */
+
+		// 新增用户
+		/*
+		 * User user=new User(); user.setUserId(UUID.randomUUID().toString());
+		 * user.setUserName("zkn"); user.setPassword("111");
+		 * DbUtil.addUser(user);
+		 */
+
+		// 根据id查询用户
+		/*
+		 * User u = DbUtil.queryUsers("2009620d-32ae-472f-becc");
+		 * System.out.println(u.getUserId()); System.out.println("姓名:" +
+		 * u.getUserName()); System.out.println("密码:" + u.getPassword());
+		 */
+
+		// 更新用户
+		User u = new User();
+		u.setUserId("2009620d-32ae-472f-becc-fdaca54ef9e9");
+		u.setPassword("123");
+		u.setUserName("mm");
+		int n = DbUtil.updateUser(u);
+		if (n == 1) {
+			System.out.println("更新成功");
+		} else {
+			System.out.println("更新失败");
+		}
+
 	}
 
 	public static boolean getUserByName(String userName, String password) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		User user = new User();
-		String URL = "jdbc:mysql://127.0.0.1:3306/approval";
-		String jdbc_user = "root";
-		String jdbc_password = "111111";
-		
-		// 加载驱动程序
-		Class.forName("com.mysql.jdbc.Driver");
-		// 获取数据库连接
-		Connection con = DriverManager.getConnection(URL, jdbc_user, jdbc_password);
+		Connection con = getConnection();
 		// 创建Statament对象
 		Statement st = con.createStatement();
 		String s = "select user_name,password from tbl_user where user_name=? and password=?";
@@ -51,78 +63,81 @@ public class DbUtil {
 		con.close();
 		return false;
 	}
-	public static int addUser(User u) throws ClassNotFoundException, SQLException{
-		String URL="jdbc:mysql://127.0.0.1:3306/approval";
-		String jdbc_user="root";
-		String jdbc_password="111111";
 
-		// 加载驱动程序
-		Class.forName("com.mysql.jdbc.Driver");
-		// 获取数据库连接
-		Connection con = DriverManager.getConnection(URL, jdbc_user, jdbc_password);
+	public static int addUser(User u) throws ClassNotFoundException, SQLException {
+		Connection con = getConnection();
 		// 创建Statament对象
 		Statement st = con.createStatement();
-		String s="insert into tbl_user(id,user_name,password) values(?,?,?)";
+		String s = "insert into tbl_user(id,user_name,password) values(?,?,?)";
 		PreparedStatement pst = con.prepareStatement(s);
-		String name=u.getUserName();
-		String pwd=u.getPassword();
-		String id=u.getUserId();
+		String name = u.getUserName();
+		String pwd = u.getPassword();
+		String id = u.getUserId();
 		pst.setString(1, id);
 		pst.setString(2, name);
 		pst.setString(3, pwd);
-		int n=pst.executeUpdate();
+		int n = pst.executeUpdate();
 		return n;
-		
+
 	}
-	
-	public static int deleteUser(String user_id) throws ClassNotFoundException, SQLException{
-		
-		String URL="jdbc:mysql://127.0.0.1:3306/approval";
-		String jdbc_user="root";
-		String jdbc_password="111111";	
-		// 加载驱动程序
-				Class.forName("com.mysql.jdbc.Driver");
-				// 获取数据库连接
-				Connection con = DriverManager.getConnection(URL, jdbc_user, jdbc_password);
-				// 创建Statament对象
-				Statement st = con.createStatement();
-				String s="delete from tbl_user where id=?";
-				PreparedStatement pst=con.prepareStatement(s);
-				pst.setString(1, user_id);
-				int n=pst.executeUpdate();
-				return n;
+
+	public static int deleteUser(String user_id) throws ClassNotFoundException, SQLException {
+
+		Connection con = getConnection();
+		// 创建Statament对象
+		Statement st = con.createStatement();
+		String s = "delete from tbl_user where id=?";
+		PreparedStatement pst = con.prepareStatement(s);
+		pst.setString(1, user_id);
+		int n = pst.executeUpdate();
+		return n;
 	}
-	public static User queryUsers(String id) throws ClassNotFoundException, SQLException{
-		String URL="jdbc:mysql://127.0.0.1:3306/approval";
-		String jdbc_user="root";
-		String jdbc_password="111111";
-		Class.forName("com.mysql.jdbc.Driver");//加载驱动程序
-		Connection con=DriverManager.getConnection(URL, jdbc_user, jdbc_password);//获取数据库连接
-		Statement st=con.createStatement();//创建Statement对象
-		String s="select id, user_name,password from tbl_user where id=?";
-		PreparedStatement pst=con.prepareStatement(s);
+
+	public static User queryUsers(String id) throws ClassNotFoundException, SQLException {
+		Connection con = getConnection();
+		Statement st = con.createStatement();// 创建Statement对象
+		String s = "select id, user_name,password from tbl_user where id=?";
+		PreparedStatement pst = con.prepareStatement(s);
 		pst.setString(1, id);
-		ResultSet rs=pst.executeQuery();
-		String user_id=null;
-		String name=null;
-		String password=null;
-		while(rs.next()){
-			user_id=rs.getString("id");
-			name=rs.getString("user_name");
-			password=rs.getString("password");
-			
-			}
-		User u=new User();
+		ResultSet rs = pst.executeQuery();
+		String user_id = null;
+		String name = null;
+		String password = null;
+		while (rs.next()) {
+			user_id = rs.getString("id");
+			name = rs.getString("user_name");
+			password = rs.getString("password");
+
+		}
+		User u = new User();
 		u.setUserId(user_id);
 		u.setUserName(name);
 		u.setPassword(password);
-		
+
 		rs.close();
 		pst.close();
 		con.close();
 		return u;
-	
-		
+
 	}
 
+	private static Connection getConnection() throws ClassNotFoundException, SQLException {
+		String URL = "jdbc:mysql://127.0.0.1:3306/approval";
+		String jdbc_user = "root";
+		String jdbc_password = "111111";
+		Class.forName("com.mysql.jdbc.Driver");// 加载驱动程序
+		Connection con = DriverManager.getConnection(URL, jdbc_user, jdbc_password);// 获取数据库连接
+		return con;
+	}
+
+	public static int updateUser(User u) throws ClassNotFoundException, SQLException {
+		Connection con = getConnection();
+		String s = "update tbl_user set user_name=?,password=? where id=?";
+		PreparedStatement pst = con.prepareStatement(s);
+	    pst.setString(1, u.getUserName());
+		pst.setString(2, u.getPassword());
+		pst.setString(3, u.getUserId());
+		return pst.executeUpdate();
+
+	}
 }
